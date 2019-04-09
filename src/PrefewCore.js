665d460068297117d94a51b32b8240806090f38f
Jaid <jaid.jsx@gmail.com>
2019-04-09 10:24:19 +0200

    Started refactoring for performance improvements
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/PrefewCore.js
+++ src/PrefewCore.js
@@ -7,7 +7,8 @@ import parsePath from "lib/parsePath"
 import EventEmitter from "eventemitter3"
 import chokidar from "chokidar"
 import jimp from "src/jimp"
-import {isString, isBuffer, mapValues, isEmpty} from "lodash"
+import {isString, isBuffer, isEmpty} from "lodash"
+import PCancelable from "p-cancelable"
 
 import SocketServer from "./SocketServer"
 
@@ -37,7 +38,8 @@ export default class extends EventEmitter {
     const presetRequire = require.context("./presets/", true, /index\.js$/)
     return presetRequire.keys().reduce((state, value) => {
       const presetName = value.match(/\.\/(?<key>[\da-z]+)\//i).groups.key
-      state[presetName] = presetRequire(value).default
+      const Preset = presetRequire(value).default
+      state[presetName] = new Preset
       return state
     }, {})
   }
@@ -102,41 +104,81 @@ export default class extends EventEmitter {
   }
 
   async updateImagePreviews(imageName) {
-    const image = this.images[imageName]
     const interestedClients = this.getClientsForImage(imageName)
     if (interestedClients |> isEmpty) {
       return
     }
     debug("Determined %d interested clients", interestedClients.length)
     for (const client of interestedClients) {
-      client.socketClient.emit("startingRender")
-      if (image.type === "file" && !image.buffer) {
-        image.buffer = await fsp.readFile(image.imagePath)
+      if (client.renderJob) {
+        debug("There is already a job running. Trying to overwrite.")
+        client.renderJob.cancel()
       }
-      const sharpImage = sharp(image.buffer)
-      const forcedOptions = {
-        ...image.forcedOptions,
-      }
-      const jobs = client.options.presets.map(async requestedPreset => {
-        const presetOptions = {
-          ...requestedPreset.options,
-          ...forcedOptions,
-        }
-        const renderedBuffer = await this.render(sharpImage, requestedPreset.name, presetOptions)
-        return {
-          presetOptions,
-          buffer: renderedBuffer,
-          image: imageName,
-          presetName: requestedPreset.name,
-          time: Number(new Date),
-        }
-      })
-      const renderedBuffers = await Promise.all(jobs)
-      client.socketClient.emit("newPreview", renderedBuffers)
-      for (const mirrorClient of this.getClientsByMode("mirror")) {
-        mirrorClient.socketClient.emit("newPreview", renderedBuffers)
-      }
+      const renderJob = this.renderPreviewsForClientCancelable(client)
+      client.renderJob = renderJob
+      await renderJob
     }
+  }
+
+  renderPreviewsForClientCancelable(client) {
+    const jobWrapper = PCancelable.fn(onCancel => {
+      // onCancel.shouldReject = false
+      onCancel(() => {
+        debug("Cancelled")
+      })
+      return this.renderPreviewsForClient(client)
+    })
+    const renderJob = jobWrapper()
+    return renderJob
+  }
+
+  async renderPreviewsForClient(client) {
+    client.socketClient.emit("startingRender")
+    const imageName = client.options.image
+    const image = this.images[imageName]
+    if (image.type === "file" && !image.buffer) {
+      debug("Initially reading file contents for image %s", imageName)
+      image.buffer = await fsp.readFile(image.imagePath)
+    }
+    const forcedOptions = {
+      ...image.forcedOptions,
+    }
+    const jobPayloads = client.options.presets.map(requestedPreset => {
+      const preset = this.presets[requestedPreset.name]
+      return {
+        preset,
+        imageName,
+        presetName: requestedPreset.name,
+        buffer: image.buffer,
+        options: {
+          ...preset.defaultOptions, // Options set by preset
+          ...requestedPreset.options, // Options set by user
+          ...forcedOptions, // Options set by image
+        },
+      }
+    })
+    const renderedBuffers = await this.createRenderWorker(jobPayloads)
+    client.socketClient.emit("newPreview", renderedBuffers)
+    for (const mirrorClient of this.getClientsByMode("mirror")) {
+      mirrorClient.socketClient.emit("newPreview", renderedBuffers)
+    }
+    client.renderJob = null
+  }
+
+  createRenderWorker(jobPayloads) {
+    const jobs = jobPayloads.map(async jobPayload => {
+      const startTime = Number(new Date)
+      const renderedBuffer = await this.render(jobPayload.buffer, jobPayload.preset.render, jobPayload.options)
+      return {
+        startTime,
+        presetOptions: jobPayload.options,
+        buffer: renderedBuffer,
+        image: jobPayload.imageName,
+        presetName: jobPayload.presetName,
+        endTime: Number(new Date),
+      }
+    })
+    return Promise.all(jobs)
   }
 
   async updateProviderImage(name, properties) {
@@ -183,14 +225,8 @@ export default class extends EventEmitter {
     debug("Client %s disconnected, %d clients are connected now", clientId, Object.keys(this.clients).length)
   }
 
-  async render(source, presetName, presetOptions) {
-    const preset = this.presets[presetName]
-    const defaultOptions = preset.options ? mapValues(preset.options, ({defaultValue}) => defaultValue) : {}
-    const mergedOptions = {
-      ...defaultOptions,
-      ...presetOptions,
-    }
-    debug("Rendering %s with preset \"%s\" and options %o", isString(source) ? source : "buffer", preset.name, mergedOptions)
+  async render(source, renderPromise, options) {
+    // debug("Rendering %s with preset \"%s\" and options %o", isString(source) ? source : "buffer", preset.name, mergedOptions)
     const sourceSharp = do {
       if (isString(source)) {
         sharp(source)
@@ -201,24 +237,23 @@ export default class extends EventEmitter {
       }
     }
     let sharpImage
-    if (mergedOptions.skipTrimming) {
-      debug("Skipping trimming")
+    if (options.skipTrimming) {
       sharpImage = sourceSharp
     } else {
       const loadedImageBuffer = await sourceSharp.png({compressionLevel: 0}).toBuffer()
       const jimpImage = await jimp.read(loadedImageBuffer)
       const croppedBuffer = await jimpImage
         .deflateLevel(0) // Skipping compression
-        .cropTransparent(mergedOptions.cropTolerance)
+        .cropTransparent(options.cropTolerance)
         .getBufferAsync(jimp.MIME_PNG)
       sharpImage = sharp(croppedBuffer)
     }
-    let processedImage = await preset.render(this, sharpImage, mergedOptions)
-    if (mergedOptions.pixelZoom > 1) {
+    let processedImage = await renderPromise(this, sharpImage, options)
+    if (options.pixelZoom > 1) {
       const renderedBuffer = await processedImage.png({compressionLevel: 0}).toBuffer()
       const newSharp = sharp(renderedBuffer)
       const {width, height} = await newSharp.metadata()
-      processedImage = newSharp.resize(width * mergedOptions.pixelZoom, height * mergedOptions.pixelZoom, {kernel: "nearest"})
+      processedImage = newSharp.resize(width * options.pixelZoom, height * options.pixelZoom, {kernel: "nearest"})
     }
     return processedImage
       .webp({
--- src/SocketServer.js
+++ src/SocketServer.js
@@ -14,7 +14,7 @@ export default class SocketServer {
       }
       if (profile.mode === "user") {
         client.emit("hey", {
-          presets: mapValues(prefewCore.presets, preset => pick(preset, "id", "name", "options")),
+          presets: mapValues(prefewCore.presets, preset => pick(preset, "title", "optionsSchema")),
           images: mapValues(prefewCore.images, image => ({
             extension: image.extension,
             thumbnail: image.thumbnail,
@@ -23,7 +23,7 @@ export default class SocketServer {
         })
       } else if (profile.mode === "mirror") {
         client.emit("hey", {
-          presets: mapValues(prefewCore.presets, preset => pick(preset, "id", "name", "options")),
+          presets: mapValues(prefewCore.presets, preset => pick(preset, "title", "optionsSchema")),
           images: mapValues(prefewCore.images, ({extension}) => ({extension})),
         })
       }
